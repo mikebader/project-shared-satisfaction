bb64d7eb226ef9a6fc2803f90a2f584c938a9597
Mike Bader <michaeldmbader@gmail.com>
2019-04-13 18:44:26 -0400

    Updated function to estimate and combine predictions

--- analysis/make_projections.R
+++ analysis/make_projections.R
@@ -6,16 +6,42 @@ newdata <- data.frame(dem.race=c('white','api','black','latino'),
                       educ='B.A.', nhdsize='1-9 blocks',
                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
                       forborn=d['forborn','mean'], man=d['man','mean'],
-                      kids=d['kids','mean'], married=d['married','mean']
-                      )
+                      kids=d['kids','mean'], married=d['married','mean'],
+                      satisfied=as.numeric(coef(mean_satisfied)))
 newdata <- data.frame(dem.race=c('white','api','black','latino'),
                       sample_tract="51059452600",
                       educ='B.A.', nhdsize='1-9 blocks',
                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
                       forborn=FALSE, man=FALSE,
-                      kids=FALSE, married=FALSE
+                      kids=FALSE, married=FALSE,
+                      satisfied=0
 )
-mfac_test <- with(dcassvy, svyglm(satisfied ~ dem.race + sample_tract + educ + nhdsize+
-                                      age + nhdyrs + I(forborn*1) + I(man*1) +
-                                      I(kids*1) + I(married*1), family=binomial()))
-predict(mfac_test$imp1, newdata=newdata, type='response')
+
+MIpredict <- function(mlist) {
+    newdata <- data.frame(dem.race=c('white','api','black','latino'),
+                          sample_tract="51059452600",
+                          educ='B.A.', nhdsize='1-9 blocks',
+                          age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
+                          forborn=d['forborn','mean'], man=d['man','mean'],
+                          kids=d['kids','mean'], married=d['married','mean'],
+                          satisfied=0)
+    for (i in mlist) {
+        for(b in c('forborn', 'man', 'kids', 'married')) {
+            idx <- grep(b, names(i$coefficients))
+            if(length(idx)>0) names(i$coefficients['idx']) <- b
+        }
+    }
+    preds <- lapply(mlist, predict, newdata=newdata, type='link', se=TRUE,
+                    interval='confidence', vcov=TRUE) %>%
+        as.data.frame()
+    print(preds)
+    betas <- apply(preds[seq(1,9,2)], 1, mean)
+    v.in <- apply(preds[seq(2,10,2)]^2,1,mean)
+    v.bn <- apply((preds[seq(1,9,2)]-betas)^2, 1, sum)/4
+    Vs <- v.in + v.bn*(1+1/5)
+    mi <- data.frame(beta=betas, se=sqrt(Vs))
+    mi$P <- plogis(mi$beta)
+    mi$lci <- plogis(mi$beta - (1.96*mi$se))
+    mi$uci <- plogis(mi$beta + (1.96*mi$se))
+    return(mi)
+}
