1ec41484de88e3a93dc1af96fc0bb04b6251c6c2
Mike Bader <michaeldmbader@gmail.com>
2019-04-24 15:55:40 -0400

    Centered all independent variables at zero for predictions

--- analysis/make_projections.R
+++ analysis/make_projections.R
@@ -1,34 +1,11 @@
 ### Need to make this for models in analysis
 ### Apply Rubin's Rules to the predictions to calculate adjusted SEs
 
-# newdata <- data.frame(dem.race=c('white','api','black','latino'),
-#                       sample_tract="51059452600",
-#                       educ='B.A.', nhdsize='1-9 blocks',
-#                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
-#                       forborn=d['forborn','mean'], man=d['man','mean'],
-#                       kids=d['kids','mean'], married=d['married','mean'],
-#                       satisfied=as.numeric(coef(mean_satisfied)))
-# newdata <- data.frame(dem.race=c('white','api','black','latino'),
-#                       sample_tract="51059452600",
-#                       educ='B.A.', nhdsize='1-9 blocks',
-#                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
-#                       forborn=FALSE, man=FALSE,
-#                       kids=FALSE, married=FALSE,
-#                       satisfied=0
-# )
-
 MIpredict <- function(mlist) {
-    # newdata <- data.frame(dem.race=c('white','api','black','latino'),
-    #                       sample_tract="51059452600",
-    #                       educ='B.A.', nhdsize='1-9 blocks',
-    #                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
-    #                       forborn=d['forborn','mean'], man=d['man','mean'],
-    #                       kids=d['kids','mean'], married=d['married','mean'],
-    #                       satisfied=0)
     newdata <- data.frame(dem.race=c('white','api','black','latino'),
                           sample_tract="51059452600",
                           educ='B.A.', nhdsize='1-9 blocks',
-                          age=50, nhdyrs=7,
+                          age=0, nhdyrs=0,
                           forborn=FALSE, man=FALSE,
                           kids=FALSE, married=FALSE,
                           satisfied=0,
