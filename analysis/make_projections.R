173d98534987f1d605c2bddae03e74b93929b674
Mike Bader <michaeldmbader@gmail.com>
2019-04-24 15:43:32 -0400

    Streamlined code and added robustness section to analysis

--- analysis/make_projections.R
+++ analysis/make_projections.R
@@ -1,30 +1,41 @@
 ### Need to make this for models in analysis
 ### Apply Rubin's Rules to the predictions to calculate adjusted SEs
 
-newdata <- data.frame(dem.race=c('white','api','black','latino'),
-                      sample_tract="51059452600",
-                      educ='B.A.', nhdsize='1-9 blocks',
-                      age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
-                      forborn=d['forborn','mean'], man=d['man','mean'],
-                      kids=d['kids','mean'], married=d['married','mean'],
-                      satisfied=as.numeric(coef(mean_satisfied)))
-newdata <- data.frame(dem.race=c('white','api','black','latino'),
-                      sample_tract="51059452600",
-                      educ='B.A.', nhdsize='1-9 blocks',
-                      age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
-                      forborn=FALSE, man=FALSE,
-                      kids=FALSE, married=FALSE,
-                      satisfied=0
-)
+# newdata <- data.frame(dem.race=c('white','api','black','latino'),
+#                       sample_tract="51059452600",
+#                       educ='B.A.', nhdsize='1-9 blocks',
+#                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
+#                       forborn=d['forborn','mean'], man=d['man','mean'],
+#                       kids=d['kids','mean'], married=d['married','mean'],
+#                       satisfied=as.numeric(coef(mean_satisfied)))
+# newdata <- data.frame(dem.race=c('white','api','black','latino'),
+#                       sample_tract="51059452600",
+#                       educ='B.A.', nhdsize='1-9 blocks',
+#                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
+#                       forborn=FALSE, man=FALSE,
+#                       kids=FALSE, married=FALSE,
+#                       satisfied=0
+# )
 
 MIpredict <- function(mlist) {
+    # newdata <- data.frame(dem.race=c('white','api','black','latino'),
+    #                       sample_tract="51059452600",
+    #                       educ='B.A.', nhdsize='1-9 blocks',
+    #                       age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
+    #                       forborn=d['forborn','mean'], man=d['man','mean'],
+    #                       kids=d['kids','mean'], married=d['married','mean'],
+    #                       satisfied=0)
     newdata <- data.frame(dem.race=c('white','api','black','latino'),
                           sample_tract="51059452600",
                           educ='B.A.', nhdsize='1-9 blocks',
-                          age=d['age','mean'], nhdyrs=d['nhdyrs','mean'],
-                          forborn=d['forborn','mean'], man=d['man','mean'],
-                          kids=d['kids','mean'], married=d['married','mean'],
-                          satisfied=0)
+                          age=50, nhdyrs=7,
+                          forborn=FALSE, man=FALSE,
+                          kids=FALSE, married=FALSE,
+                          satisfied=0,
+                          nhd.srch.herndon.live=FALSE,
+                          nhd.srch.wheaton.live=FALSE,
+                          nhd.srch.germantown.live=FALSE
+    )
     for (i in mlist) {
         for(b in c('forborn', 'man', 'kids', 'married')) {
             idx <- grep(b, names(i$coefficients))
@@ -34,7 +45,6 @@ MIpredict <- function(mlist) {
     preds <- lapply(mlist, predict, newdata=newdata, type='link', se=TRUE,
                     interval='confidence', vcov=TRUE) %>%
         as.data.frame()
-    print(preds)
     betas <- apply(preds[seq(1,9,2)], 1, mean)
     v.in <- apply(preds[seq(2,10,2)]^2,1,mean)
     v.bn <- apply((preds[seq(1,9,2)]-betas)^2, 1, sum)/4
--- analysis/report_models.R
+++ analysis/report_models.R
@@ -10,6 +10,7 @@ report_models <- function(...,reglabels=c(), label='', caption='') {
     ht <- huxreg(..., statistics='', align='.', borders=0)
     ht <- ht[-1*fe_rows(ht),]
     ht[seq(4,(length(reglabels)+1)*2,2), 1] <- reglabels
+    last.row <- nrow(ht)
     ht <- ht %>%
         insert_row(c('Race','','','',''),
                      after=(which(ht$names=="Asian")-1)) %>%
@@ -20,7 +21,7 @@ report_models <- function(...,reglabels=c(), label='', caption='') {
         insert_row(c('Neighborhood perceptions','','','',''),
                    after=which(ht$names=="B.A.")+4) %>%
         insert_row(c('AIC', sapply(models, function(x) round(x$aic$mean, 3))),
-                   after=which(ht$names==">50 blocks")+5) %>%
+                   after=nrow(ht)+3) %>%
         set_bottom_border(row=c(1,length(ht$names)+4, length(ht$names)+3), col=everywhere, value=1) %>%
         set_italic(which(ht$names %in% section_labels), everywhere, TRUE) %>%
         set_font_size(9.5) %>%
