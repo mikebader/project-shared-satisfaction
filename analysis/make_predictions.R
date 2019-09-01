4c5303d60b8dc4c7c1d92a1eaf55a0cf0a0d9a1f
Mike Bader <michaeldmbader@gmail.com>
2019-09-01 11:06:21 -0400

    Fixed inefficiency and incorrect method of combining AMEs

--- analysis/make_predictions.R
+++ analysis/make_predictions.R
@@ -1,37 +1,70 @@
 library(prediction)
 
-## Apply Rubin's rules to combine estimates and standard errors of
-## average marginal effects (AMEs) across imputations
+## get.margins; fit = single fitted model
+## Returns AMEs, margins and fitted values whites on the link scale for
+## a fitted model
+get.margins <- function(fit) {
+    res <- list()
 
-## **IT IS COMPUTATIONALLY INEFFICIENT TO RUN `margins()` TWICE, CAN'T
-## FIGURE OUT A BETTER WAY**
-MIame <- function(modelset) {
-    fitnames <- names(modelset)
-    amelist <- lapply(fitnames, function(fit) summary(margins(modelset[[fit]], variables='dem.race', design=dcassvy$designs[[fit]])))
-    prs <- sapply(amelist, function(x) x$AME)
-    means <- apply(prs, 1, mean)
-    return(means)
+    ## Set baseline data to everyone being white
+    d0 <- fit$data
+    d0$dem.race <- 'white'
+
+    ## Predict outcome on link scale when all respondents are white
+    pred0 <- prediction(fit, data=d0, type='link')
+
+    ## Calculate marginal effects on link scale for other races
+    m <- margins(fit, variables='dem.race', data=des$variables,
+                 design=des, type='link')
+    ame <- summary(m)
+    ame$V <- ame$SE^2
+
+    ## Return values
+    res$ame <- ame      ## Summarized AMEs
+    res$margins <- m    ## Calculated margins
+    res$pred0 <- pred0  ## Average predicted values for whites
+    return(res)
 }
 
-MIame_pval <- function(modelset) {
-    amelist <- lapply(seq_along(modelset), function(fit) {
-        ame <- summary(
-            margins(modelset[[fit]], variables='dem.race',
-                    design=dcassvy$designs[[fit]], type='link')
-            )
-        ame$V <- ame$SE^2
-        return(ame)
-        })
-    pwide <- as.data.frame(amelist)
-    AME.cols <- grep('^AME', names(pwide))
-    V.cols <- grep('^V', names(pwide))
+## get.predictions; res = get.margins() object
+## Calculates probabilities based on summing individual marginal effects
+## with predicted outcomes for whites
+get.predictions <- function(res) {
+    pred0 <- res$pred0
+    mlink <- res$margins
+    N <- length(pred0$fitted)
+    fitted <- data.frame(white=rep(0,N),
+                         mlink[grep('^dydx_', names(mlink))])
+    mprobs <- sapply(fitted, function(x) {
+        stats::weighted.mean(plogis(x+pred0$fitted), w=des$variables$weight)
+    })
+    names(mprobs) <- gsub('^dydx_dem\\.race','',names(mprobs))
+    return(mprobs)
+}
 
-    pwide$mean <- apply(pwide[, AME.cols], 1, mean)
+## calculate.pvals; amelist = list of get.margins() objects
+## Returns AMEs on link scale with p-values based on combining multiply
+## imputed variances, predicted probabilities, and AMEs on
+## response scale
+calculate.pvals <- function(amelist) {
+    res <- list()
+    AMEs <- lapply(amelist, function(x) x$ame)
+    pwide <- as.data.frame(AMEs)
+
+    AME.cols <- grep('AME$', names(pwide))
+    V.cols <- grep('V$', names(pwide))
+
+    # pwide$dem.race <- sub('dem\\.race','', pwide[,1])
+    pwide$AMEmi <- apply(pwide[, AME.cols], 1, mean)
     pwide$Vin <- apply(pwide[, V.cols], 1, mean)
     pwide$Vbn <- (1/length(modelset))*apply((pwide[, V.cols]-pwide$Vin)^2, 1, sum)
     pwide$Vmi <- pwide$Vin + (1 + (1/length(modelset)))*pwide$Vbn
-    pwide$pval <- pnorm(abs(pwide$mean/sqrt(pwide$Vmi)), lower.tail = FALSE)
-    return(pwide$pval)
+    pwide$pmi <- pnorm(abs(pwide$AMEmi/sqrt(pwide$Vmi)), lower.tail = FALSE)*2
+
+    res$AME_link <- pwide[, c('AMEmi','Vin','Vbn','Vmi','pmi')]
+    res$pred <- apply(sapply(amelist, get.predictions), 1, mean)
+    res$AME_resp <- res$pred[1:4] - res$pred[1]
+    return(res)
 }
 
 
