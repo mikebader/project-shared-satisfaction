521dc9c5407eb79ac74e84d409b4727c65ba074e
Mike Bader <michaeldmbader@gmail.com>
2020-06-05 09:00:33 -0400

    Initial comparitive analysis of multiracial vs. DC-area residents
    * Adds analyses of satisfaction in multiracial neighborhoods and among all DC-area residents
    * Creates analytical tables and adds them to multiethnic-nhoods-tables.tex
    * Improves tidy/broom functions for reporting multiply-imputed survey data
    * Generalizes report_models() function to work with comparison model tables in report_models.R
    * Generalizes MIpredict() to predict values at the intercept in addition to dataset means & modes in make_predictions.R

--- analysis/make_predictions.R
+++ analysis/make_predictions.R
@@ -79,27 +79,42 @@ calculate.pvals <- function(amelist) {
 ## Function to calculate probabilities using mean/mode of non-race
 ## variables and tract number '51059452600' (median value of satisfaction
 ## among tracts) for a fit model on the logistic scale
-make.prediction <- function(fit) {
+make.prediction <- function(fit, vars, at = "mean_or_mode") {
     modelvars <- names(attr(fit$terms, 'dataClasses'))
-    vars <- c('age', 'forborn', 'man', 'kids', 'married', 'educ',
-              'nhdsize', 'nhdyrs')
     p.vars <- intersect(modelvars, vars)
-    newdata <- data.frame(sample_tract='51059452600',
-                          dem.race=c('white','api','black','latino'))
-    if(length(p.vars)>0) newdata <- data.frame(
-        newdata, mean_or_mode(fit$data[, p.vars]))
+    newdata <- data.frame(sample_tract=levels(dcas16$sample_tract)[1],
+                          raceeth=c('white','asian','black','latino'))
+    if(at == "mean_or_mode") {
+        if(length(p.vars)>0) newdata <- data.frame(
+            newdata, mean_or_mode(fit$data[, p.vars]))
+    }
+    if(at == "intercept") {
+        if(length(p.vars)>0) {
+            zeros <- matrix(rep(0, length(p.vars)*nrow(newdata)),
+                               ncol=length(p.vars))
+            colnames(zeros) <- p.vars
+            newdata <- cbind(newdata, as.data.frame(zeros))
+        }
+
+    }
+    # print(head(newdata))
     Ps <- predict(fit, newdata=newdata, type='link', se.fit=TRUE) %>%
         as.data.frame()
     Ps$V <- Ps$SE^2
-    Ps$dem.race <- c('white','api','black','latino')
-    return(Ps[, c('dem.race', 'link', 'V')])
+    Ps$raceeth <- c('white','asian','black','latino')
+    return(Ps[, c('raceeth', 'link', 'V')])
 }
 
 ## pr.values; modelset=set of fit models from multiple imputation
 ## Function that returns predicted probability and confidence intervals
 ## combining multiple imputation results using Rubin's rules
-MIpredict <- function(modelset) {
-    pwide <- cbind(as.data.frame(lapply(modelset, make.prediction)))
+MIpredict <- function(
+    modelset,
+    vars = c('age', 'forborn', 'man', 'kids', 'married', 'educ',
+             'nhdsize', 'nhdyrs'),
+    at = "mean_or_mode"
+) {
+    pwide <- cbind(as.data.frame(lapply(modelset, make.prediction, vars, at)))
     yhat.cols <- grep('link$', names(pwide))
     V.cols <- grep('V$', names(pwide))
 
@@ -111,7 +126,7 @@ MIpredict <- function(modelset) {
     pwide$lci <- plogis(pwide$mean - 1.96*sqrt(pwide$Vmi))
     pwide$uci <- plogis(pwide$mean + 1.96*sqrt(pwide$Vmi))
 
-    return(data.frame(dem.race=c('white','api','black','latino'),
+    return(data.frame(raceeth=c('white','asian','black','latino'),
                       pwide[, c('p', 'lci', 'uci')]))
 }
 
