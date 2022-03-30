34a12708ea1cf11cc8ee063cd1846562ac98d372
mikebader <mikebader@mikebader.net>
2022-03-30 11:01:55 -0400

    Copy files to repository
    Remove simlink to files and create hard copy for replication purposes.
    
    Construction of original files can be found at https://git.mikebader.net/mikebader/data-nhgis-dcarea

--- analysis/dcarea/dcarea_functions.R
+++ analysis/dcarea/dcarea_functions.R
@@ -1,1 +1,34 @@
-/Users/bader/work/data/nhgis/DCArea/dcarea_functions.R
+## Functions to create data for DC Area
+## (DC & surrounding jurisdictions, including independent cities)
+
+select.dcarea <- function(dta) {
+## Arguments: `dta`: dataset from from which to select observations in
+##                   the DC Area
+## Returns: dataset containing only observations from the DC area
+
+    ## Identify counties to keep in DC Area dataset using FIPS county codes
+    counties <- c(
+          '110001'  # D.C.
+        , '240031'  # Montgomery County
+        , '240033'  # Prince George's County
+        , '510013'  # Arlington County
+        , '510059'  # Fairfax County
+        , '510510'  # Alexandria city
+        , '510600'  # Fairfax city
+        , '510610'  # Falls Church city
+    )
+
+    ## Select counties using regular expression based on FIPS codes above
+    ## and common `GISJOIN` variable
+    re <- paste0('^G', counties, collapse = '|')
+    dcarea <- dta[grep(re, dta$GISJOIN, perl=TRUE),]
+
+    ## Replace factor variables to contain only levels in the DC area
+    if('COUNTY' %in% names(dcarea)) {
+        dcarea$COUNTY <- factor(dcarea$COUNTY)
+    }
+    if('STATE' %in% names(dcarea)) {
+        dcarea$STATE <- factor(dcarea$STATE)
+    }
+    return(dcarea)
+}
